9d39ed8e325ca1cb2c13097d2dcc7111bbfe34f5
Burminskyi <burminskyi91@gmail.com>
2023-12-26 17:16:44 +0200

    Added redirect to mainPage if authentificated

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux";
 import { AuthSection } from "../components/AuthSection";
 import Description from "../components/Description";
+import { selectAuthenticationStatus } from "../redux/auth/selectors";
+import { Navigate } from "react-router-dom";
 
 export const MainPage = () => {
+  const authenticated = useSelector(selectAuthenticationStatus);
+  if (authenticated) return <Navigate to="/calendar" />;
   return (
     <>
       <AuthSection />
